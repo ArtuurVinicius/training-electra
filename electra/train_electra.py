f72d5520b56fd3a73149802fb8cd892a3e8a337b
ArtuurVinicius <lartur671@gmail.com>
2026-06-09 09:02:10 -0300

    feat: novo treinamento do electra

--- electra/train_electra.py
+++ electra/train_electra.py
@@ -84,9 +84,9 @@ def build_fast_tokenizer_from_bpe_files(tokenizer_dir, max_seq_length):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset_path', type=str, default='./dataset/dataset_labeled.csv')
+    parser.add_argument('--dataset_path', type=str, default='./dataset/final_corpus.csv')
     parser.add_argument('--text_column', type=str, default='content')
-    parser.add_argument('--output_dir', type=str, default='./electra/electra_output')
+    parser.add_argument('--output_dir', type=str, default='./electra/electra_output_final')
     parser.add_argument('--tokenizer_dir', type=str, default='./electra/tokenizer')
     parser.add_argument('--train_tokenizer', action='store_true')
     parser.add_argument('--vocab_size', type=int, default=30000)
